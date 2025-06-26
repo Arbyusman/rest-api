69fff39263c6bd6afbcc6b44c72ddfafaf28b1f3
arbyusman <acingman13@gmail.com>
2025-06-26 23:22:48 +0800

    chore: store user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use App\Traits\SiteTrait;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -22,9 +20,10 @@ class UserController extends Controller
     {
         try {
             $validated = $request->validated();
+            $validated['email_verified_at'] = now();
             $user = User::create($validated);
 
-            return $this->jsonResponse(201, 'User created successfully', $user);
+            return $this->jsonResponse(201, 'User created successfully', [$user]);
         } catch (\Exception $e) {
             return $this->jsonResponse(500, $e->getMessage());
         }
