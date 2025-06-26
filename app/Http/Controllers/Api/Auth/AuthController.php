abe16fb1b538cf359bc4f8444808e45a827d8e0a
arbyusman <acingman13@gmail.com>
2025-06-26 17:56:33 +0800

    feat: auth login and logout

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,57 +5,46 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
 use App\Models\User;
-use App\Traits\ResponseTrait;
 use App\Traits\SiteTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-
     use SiteTrait;
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(AuthRequest $request)
+    public function login(AuthRequest $request)
     {
         try {
-            $data =  $request->validated();
-            User::create($data);
-            return $this->JsonResponse('User created successfully', 201);
+            $data = $request->validated();
+
+            $user = User::where('email', $data['email'])->first();
+
+            if (! $user || ! Hash::check($data['password'], $user->password)) {
+                return $this->jsonResponse(401, 'The provided credentials are incorrect.');
+            }
+
+            $token = $user->createToken($data['email'])->plainTextToken;
+
+            return $this->jsonResponse(200, 'User login successfully', [
+                'token' => $token,
+            ]);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'Failed to create user: ' . $e->getMessage()], 500);
+            return $this->jsonResponse(500, 'Failed to login', [
+                'error' => $e->getMessage(),
+            ]);
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function logout()
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        try {
+            auth('sanctum')->user()->currentAccessToken()->delete();
+            return $this->jsonResponse(200, 'Logout successful');
+        } catch (\Exception $e) {
+            return $this->jsonResponse(500, 'Failed to logout', [
+                'error' => $e->getMessage(),
+            ]);
+        }
     }
 }
